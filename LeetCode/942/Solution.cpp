37d2679db739d63b7b27602c9d52cb031638723c
SeungYoon Kim <dskym@naver.com>
2019-07-23 23:43:55 +0900

    20190723

--- LeetCode/942/Solution.cpp
+++ LeetCode/942/Solution.cpp
@@ -3,18 +3,13 @@ public:
     vector<int> diStringMatch(string S) {
         vector<int> result;
         
-        result.push_back(0);
+        int s = 0, m = S.length();
         
-        for(int i=S.length()-1;i>=0;--i) {
-            if(S[i] == 'I') {                
-                for(int j=0;j<result.size();++j)
-                    result[j] += 1;
-                
-                result.insert(result.begin(), 0);
-            }
-            else {
-                result.insert(result.begin(), result.size());
-            }
+        for(int i=0;i<=S.length();++i) {
+            if(S[i] == 'I')
+                result.push_back(s++);
+            else
+                result.push_back(m--);
         }
         
         return result;
