3ec4def3500da8fadf190c4dd1ca081c9b0968fc
SeungYoon Kim <dskym@naver.com>
2019-07-24 23:35:26 +0900

    20190724

---
+++ LeetCode/1116/Solution.cpp
@@ -0,0 +1,44 @@
+class ZeroEvenOdd {
+private:
+    int n;
+    mutex m1;
+    mutex m2;
+    mutex m3;
+public:
+    ZeroEvenOdd(int n) {
+        this->n = n;
+        
+        m2.lock();
+        m3.lock();
+    }
+
+    // printNumber(x) outputs "x", where x is an integer.
+    void zero(function<void(int)> printNumber) {
+        for(int i=1;i<=n;++i) {
+            m1.lock();
+            
+            printNumber(0);
+            
+            if(i % 2 == 1)
+                m2.unlock();
+            else
+                m3.unlock();
+        }
+    }
+
+    void even(function<void(int)> printNumber) {        
+        for(int i=2;i<=n;i+=2) {
+            m3.lock();
+            printNumber(i);
+            m1.unlock();
+        }
+    }
+
+    void odd(function<void(int)> printNumber) {
+        for(int i=1;i<=n;i+=2) {
+            m2.lock();
+            printNumber(i);
+            m1.unlock();
+        }
+    }
+};
