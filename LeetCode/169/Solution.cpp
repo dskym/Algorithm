511886ed9cfc0ecea8b2646fd69c1f7f68888f9b
SeungYoon Kim <dskym@naver.com>
2019-09-12 18:17:02 +0900

    20190912

--- LeetCode/169/Solution.cpp
+++ LeetCode/169/Solution.cpp
@@ -3,21 +3,6 @@ public:
     int majorityElement(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         
-        int limit = nums.size() / 2;
-        int count = 0;
-        int temp = nums[0];
-        
-        for(int i=1;i<nums.size();++i) {
-            if(temp != nums[i]) {
-                temp = nums[i];
-                count = 0;
-            } else
-                count++;
-            
-            if(count >= limit)
-                return temp;
-        }
-        
-        return temp;
+        return nums[nums.size()/2];
     }
 };
