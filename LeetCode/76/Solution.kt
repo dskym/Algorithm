64c58e1dd16079473f5ba2683aebe9a951909188
seungyoon-kim <dskym@naver.com>
2024-02-04 19:00:21 +0900

    20240204

--- LeetCode/76/Solution.kt
+++ LeetCode/76/Solution.kt
@@ -7,33 +7,19 @@ class Solution {
 
         var start = 0
         var end = 0
+        var count = t.length
 
         var result = ""
         while(end < s.length) {
-            counter[s[end++].toInt()]--
-            var flag = true
-            for(temp in counter) {
-                if(temp > 0) {
-                    flag = false
-                }
+            if(counter[s[end++].toInt()]-- > 0) {
+                count--
             }
-            if(flag) {
-                while(start < end) {
-                    if(result == "") {
-                        result = s.substring(start, end)
-                    } else if(result != "" && result.length > end-start) {
-                        result = s.substring(start, end)
-                    }
-                    counter[s[start++].toInt()]++
-                    var flag2 = false
-                    for(temp in counter) {
-                        if(temp > 0) {
-                            flag2 = true
-                        }
-                    }
-                    if(flag2) {
-                        break
-                    }
+            while(count == 0) {
+                if(result == "" || result.length > end-start) {
+                    result = s.substring(start, end)
+                }
+                if(++counter[s[start++].toInt()] > 0) {
+                    count++
                 }
             }
         }
