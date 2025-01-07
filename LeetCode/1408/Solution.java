d895ee9a38f06e190566f7ac9b979da10522af4c
seungyoon-kim <dskym@naver.com>
2025-01-07 21:48:23 +0900

    20250107

---
+++ LeetCode/1408/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> result = new ArrayList<>();
+        for(String word1 : words) {
+            for(String word2 : words) {
+                if(word1.equals(word2) == false && word2.indexOf(word1) != -1) {
+                    result.add(word1);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
