ab674e5baeb365ede83fd8260c3877ffc55d879a
seungyoon-kim <dskym@naver.com>
2025-05-07 21:50:16 +0900

    20250507

---
+++ LeetCode/3341/Solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minTimeToReach(int[][] moveTime) {
+        int n = moveTime.length;
+        int m = moveTime[0].length;
+
+        boolean[][] visited = new boolean[n][m];
+        int[][] dp = new int[n][m];
+        int[][] direct = {{0,1},{0,-1},{1,0},{-1,0}};
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[2]-b[2]);
+        pq.add(new int[]{0,0,0});
+        visited[0][0] = true;
+
+        while(!pq.isEmpty()) {
+            int[] node = pq.poll();
+            int x = node[0];
+            int y = node[1];
+            int time = node[2];
+
+            dp[x][y] = time;
+
+            for(int i=0;i<4;++i) {
+                int dx = x + direct[i][1];
+                int dy = y + direct[i][0];
+
+                if(dx >= 0 && dx < n && dy >= 0 && dy < m && visited[dx][dy] == false) {
+                    int newTime = Math.max(moveTime[dx][dy], dp[x][y])+1;
+                    pq.add(new int[]{dx,dy,newTime});
+                    visited[dx][dy] = true;
+                }
+            }
+        }
+
+        return dp[n-1][m-1];
+    }
+}
