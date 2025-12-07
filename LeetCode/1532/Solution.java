b043ac71d060ecfa90ce6e54f6b356ee14fde174
seungyoon-kim <dskym@naver.com>
2025-12-08 00:46:02 +0900

    20251208

---
+++ LeetCode/1532/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int countOdds(int low, int high) {
+        int result = (high-low)/2;
+        if(low%2 == 1 || high%2 == 1) {
+            result++;
+        }
+        return result;
+    }
+}
